32d13f444bc924fcaf638815c56b1408e8c430ca
hnimcognhi <89082438+hnimcognhi@users.noreply.github.com>
2021-08-17 20:00:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'biếc gòi',
     messDesc: 'đi ngủ đi',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100012665413948' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
